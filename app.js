54105ab7355f17d3a5081b1536ccda3d90c6a048
Irene1208 <72648810+Irene1208@users.noreply.github.com>
2022-07-10 14:31:01 +0800

    add index page.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.listen(PORT);
